aa086a888fe80f4e44f2ded211df402fbfd605ba
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-11-24 17:25:43 +0100

    Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import WelcomeComponent from "./components/Welcome"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Switch>
+        <Route exact path="/">
+          <WelcomeComponent />
+        </Route>
+        <Route path="/">
+          {/* <Error404 /> */}
+          ERROR 404
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// import styles from './Header.module.css'
+
+const Welcome = () => {
+    return(
+        <div>
+            <h2>Welcome !</h2>
+        </div>
+    )
+}
+
+export default Welcome
