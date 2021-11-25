a5c77bbf93c7e93f0c49c48bf9eba911d6cf0038
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-11-25 14:58:38 +0100

    Header changes depending if user is connected or not

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,22 @@ import "./style.css"
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const [isUserConnected, setIsUserConnected] = React.useState(false)
+
     return(
         <div id="header">
             <Link id="header-title" to="/"><h1>MORPIONLINE</h1></Link>
-            <div id="header-links">
-                <Link id="signin" to="/signin">Sign-in</Link>
-                <Link to="/signup">Sign-up</Link>
-            </div>
+            {isUserConnected ? 
+                (
+                    <div id="header-links">
+                        <Link to="/account">My account</Link>
+                    </div> 
+                ) : (
+                    <div id="header-links">
+                        <Link id="signin" to="/signin">Sign-in</Link>
+                        <Link to="/signup">Sign-up</Link>
+                    </div> 
+                )}
         </div>
     )
 }
