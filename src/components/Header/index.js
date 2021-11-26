4095247f649600241dc295c28c2c779962a9eeb7
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-11-26 13:03:01 +0100

    Put Shop into Logged Users header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,10 +14,10 @@ const Header = () => {
                 (
                     <div id="header-links">
                         <ButtonComponent type="link" to="/account" value="My account" />
+                        <ButtonComponent type="link" id="shop" linksTo="/shop" value="Shop" />
                     </div> 
                 ) : (
                     <div id="header-links">
-                        <ButtonComponent type="link" id="shop" linksTo="/shop" value="Shop" />
                         <ButtonComponent type="link" id="signin" linksTo="/signin" value="Sign in" />
                         <ButtonComponent type="link" linksTo="/signup" value="Sign up" />
                         
