a416dfb743884611108bad62df006fa5496b319e
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-11-29 11:23:45 +0100

    Error404 return button

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import ButtonComponent from '../ButtonComponent'
 import "./style.css"
 
 const Error404 = () => {
     return(
         <div id="error404">
             <h2>ERROR 404</h2>
-            <p>Page introuvable.</p>
+            <p>Page not found. :/</p>
+            <ButtonComponent type="link" linksTo="/" value="Back to home" />
         </div>
     )
 }
