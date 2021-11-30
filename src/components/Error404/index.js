ea7ffa015d9231468f8db688efdbf107d0475c23
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-11-30 16:15:12 +0100

    Netlify fix

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -8,7 +8,7 @@ const Error404 = () => {
             <img src="./error404.png" />
             <h2>ERROR 404</h2>
             <p>Page not found. :/</p>
-            <ButtonComponent type="link" linksTo="/" value="Back to home" />
+            <ButtonComponent type="link" linksTo="/" value="Back to home" alt="" />
         </div>
     )
 }
