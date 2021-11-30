41f462bf788c16077d186de9b84e1b9eaaccff4a
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-11-30 15:41:55 +0100

    Error404 icon

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -4,7 +4,8 @@ import "./style.css"
 
 const Error404 = () => {
     return(
-        <div id="error404">
+        <div>
+            <img src="./error404.png" />
             <h2>ERROR 404</h2>
             <p>Page not found. :/</p>
             <ButtonComponent type="link" linksTo="/" value="Back to home" />
