8728a564a01bf4a8790be61a4761e64bf8fa1529
YaniMad <yani.madebos@mmibordeaux.com>
2021-12-01 15:13:56 +0100

    correcting shopping system

--- src/components/ShopComponent/index.js
+++ src/components/ShopComponent/index.js
@@ -2,21 +2,35 @@ import React from "react";
 import ButtonComponent from "../ButtonComponent";
 import "./style.css";
 
-const buy = () => {
-  switch(product){
-    case 'croix_rouge' :
-      //pieces -= 100
-      //Stocker achat
-    break;
-    case 'croix_or' :
-      //pieces -= 100
-      //Stocker achat
-    break;
-  }
-}
-
-
 const Shop = () => {
+  const buy = (product) => {
+    switch(product){
+      case 'croix_rouge' :
+        //pieces -= 100
+        //Stocker achat
+      break;
+      case 'croix_or' :
+        //pieces -= 100
+        //Stocker achat
+      break;
+      case 'croix_verte' :
+        //pieces -= 100
+        //Stocker achat
+      break;
+      case 'croix_bleue' :
+        //pieces -= 100
+        //Stocker achat
+      break;
+      case 'croix_jaune' :
+        //pieces -= 100
+        //Stocker achat
+      break;
+      case 'croix_violette' :
+        //pieces -= 100
+        //Stocker achat
+      break;
+    }
+  }
   return (
     <div id="shop">
       <h1>Here is the Shop !</h1>
@@ -26,42 +40,42 @@ const Shop = () => {
           <div id="item-box">
             <h2>NOM ARTICLE</h2>
             <div id="article-img"></div>
-            <ButtonComponent id="buy-button" type="button" onClick={() => buy(croix_rouge)} value="Buy" />
+            <ButtonComponent id="buy-button" type="button" onClick={() => buy('croix_rouge')} value="Buy" />
           </div>
         </div>
         <div class="item-box">
           <div id="item-box">
             <h2>NOM ARTICLE</h2>
             <div id="article-img"></div>
-            <ButtonComponent id="buy-button" type="button" value=" Acheter Article" />
+            <ButtonComponent id="buy-button" type="button" onClick={() => buy('croix_or')} value="Buy" />
           </div>
         </div>
         <div class="item-box">
           <div id="item-box">
             <h2>NOM ARTICLE</h2>
             <div id="article-img"></div>
-            <ButtonComponent id="buy-button" type="button" value=" Acheter Article" />
+            <ButtonComponent id="buy-button" type="button" onClick={() => buy('croix_verte')} value="Buy" />
           </div>
         </div>
         <div class="item-box">
           <div id="item-box">
             <h2>NOM ARTICLE</h2>
             <div id="article-img"></div>
-            <ButtonComponent id="buy-button" type="button" value=" Acheter Article" />
+            <ButtonComponent id="buy-button" type="button" onClick={() => buy('croix_bleue')} value="Buy" />
           </div>
         </div>
         <div class="item-box">
           <div id="item-box">
             <h2>NOM ARTICLE</h2>
             <div id="article-img"></div>
-            <ButtonComponent id="buy-button" type="button" value=" Acheter Article" />
+            <ButtonComponent id="buy-button" type="button" onClick={() => buy('croix_jaune')} value="Buy" />
           </div>
         </div>
         <div class="item-box">
           <div id="item-box">
             <h2>NOM ARTICLE</h2>
             <div id="article-img"></div>
-            <ButtonComponent id="buy-button" type="button" value=" Acheter Article" />
+            <ButtonComponent id="buy-button" type="button" onClick={() => buy('croix_violette')} value="Buy" />
           </div>
         </div>
       </div>
