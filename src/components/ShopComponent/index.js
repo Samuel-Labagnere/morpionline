7bbf7e37e9fe08adc36c2e1cb1ab6bcf5e992ccc
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-12-03 12:50:35 +0100

    New shop interface

--- src/components/ShopComponent/index.js
+++ src/components/ShopComponent/index.js
@@ -23,51 +23,51 @@ const Shop = ({ userConnect }) => {
   
         <div id="articles">
           <div className="item-box">
-            <div id="item-box">
-              <h2>NOM ARTICLE</h2>
-              <div id="article-img"></div>
-              <p>100 coins</p>
-              <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_rouge") ? () => buy("") : () => buy('croix_rouge', 100)} value={JSON.parse(localStorage.getItem("items")).includes("croix_rouge") ? "Bought" : "Buy"} />
+            <div className="item-box">
+              <h2>RED CROSS</h2>
+              <div className="article-img" style={{display: "flex", justifyContent: "center", alignItems: "center"}}><p style={{fontWeight: "bold", fontSize: "92px", color: "red", textShadow: "0px 0px 1.5px black"}}>X</p></div>
+              <p style={{fontWeight: "bold"}}>50 coins</p>
+              <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_rouge") ? () => buy("") : () => buy('croix_rouge', 50)} value={JSON.parse(localStorage.getItem("items")).includes("croix_rouge") ? "Bought" : "Buy"} />
             </div>
           </div>
           <div className="item-box">
-            <div id="item-box">
-              <h2>NOM ARTICLE</h2>
-              <div id="article-img"></div>
-              <p>100 coins</p>
-              <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_or") ? () => buy("") : () => buy('croix_or', 100)} value={JSON.parse(localStorage.getItem("items")).includes("croix_or") ? "Bought" : "Buy"} />
+            <div className="item-box">
+              <h2>GOLDEN CROSS</h2>
+              <div className="article-img" style={{display: "flex", justifyContent: "center", alignItems: "center"}}><p style={{fontWeight: "bold", fontSize: "92px", color: "#ffd700", textShadow: "0px 0px 1.5px black"}}>X</p></div>
+              <p style={{fontWeight: "bold"}}>1500 coins</p>
+              <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_or") ? () => buy("") : () => buy('croix_or', 1500)} value={JSON.parse(localStorage.getItem("items")).includes("croix_or") ? "Bought" : "Buy"} />
             </div>
           </div>
           <div className="item-box">
-            <div id="item-box">
-              <h2>NOM ARTICLE</h2>
-              <div id="article-img"></div>
-              <p>100 coins</p>
+            <div className="item-box">
+              <h2>GREEN CROSS</h2>
+              <div className="article-img" style={{display: "flex", justifyContent: "center", alignItems: "center"}}><p style={{fontWeight: "bold", fontSize: "92px", color: "green", textShadow: "0px 0px 1.5px black"}}>X</p></div>
+              <p style={{fontWeight: "bold"}}>100 coins</p>
               <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_verte") ? () => buy("") : () => buy('croix_verte', 100)} value={JSON.parse(localStorage.getItem("items")).includes("croix_verte") ? "Bought" : "Buy"} />
             </div>
           </div>
           <div className="item-box">
-            <div id="item-box">
-              <h2>NOM ARTICLE</h2>
-              <div id="article-img"></div>
-              <p>100 coins</p>
+            <div className="item-box">
+              <h2>BLUE CROSS</h2>
+              <div className="article-img" style={{display: "flex", justifyContent: "center", alignItems: "center"}}><p style={{fontWeight: "bold", fontSize: "92px", color: "blue", textShadow: "0px 0px 1.5px black"}}>X</p></div>
+              <p style={{fontWeight: "bold"}}>100 coins</p>
               <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_bleue") ? () => buy("") : () => buy('croix_bleue', 100)} value={JSON.parse(localStorage.getItem("items")).includes("croix_bleue") ? "Bought" : "Buy"} />
             </div>
           </div>
           <div className="item-box">
-            <div id="item-box">
-              <h2>NOM ARTICLE</h2>
-              <div id="article-img"></div>
-              <p>100 coins</p>
+            <div className="item-box">
+              <h2>YELLOW CROSS</h2>
+              <div className="article-img" style={{display: "flex", justifyContent: "center", alignItems: "center"}}><p style={{fontWeight: "bold", fontSize: "92px", color: "yellow", textShadow: "0px 0px 1.5px black"}}>X</p></div>
+              <p style={{fontWeight: "bold"}}>100 coins</p>
               <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_jaune") ? () => buy("") : () => buy('croix_jaune', 100)} value={JSON.parse(localStorage.getItem("items")).includes("croix_jaune") ? "Bought" : "Buy"} />
             </div>
           </div>
           <div className="item-box">
-            <div id="item-box">
-              <h2>NOM ARTICLE</h2>
-              <div id="article-img"></div>
-              <p>100 coins</p>
-              <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_violette") ? () => buy("") : () => buy('croix_violette', 100)} value={JSON.parse(localStorage.getItem("items")).includes("croix_violette") ? "Bought" : "Buy"} />
+            <div className="item-box">
+              <h2>PURPLE CROSS</h2>
+              <div className="article-img" style={{display: "flex", justifyContent: "center", alignItems: "center"}}><p style={{fontWeight: "bold", fontSize: "92px", color: "purple", textShadow: "0px 0px 1.5px black"}}>X</p></div>
+              <p style={{fontWeight: "bold"}}>500 coins</p>
+              <ButtonComponent id="buy-button" type="button" onClick={JSON.parse(localStorage.getItem("items")).includes("croix_violette") ? () => buy("") : () => buy('croix_violette', 500)} value={JSON.parse(localStorage.getItem("items")).includes("croix_violette") ? "Bought" : "Buy"} />
             </div>
           </div>
         </div>
